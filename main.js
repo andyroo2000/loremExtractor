905a1271d79542faaf9a95aa7292952346f04863
Andrew Landry <andrewlandry@gmail.com>
2014-02-21 18:02:30 -0500

    reformat extractor object

--- main.js
+++ main.js
@@ -1,25 +1,22 @@
 var input = "4 Every Dog Must Have His 66 Every Day,";
 
-
 var extractor = {
   isNumber: function(element) {
     if (element.match(/(\d+)$/)) {
       return true;
     }
   },
+
   lorem: function(text) {
     var textArray = input.split(' ');
-
     for (var i = textArray.length - 1; i >= 0; i--) {
       if (this.isNumber(textArray[i])) {
         textArray.splice(i, 1);
       }
     }
-
     var textObj = {
       allWords: textArray
     };
-
     return textObj;
   }
 };
