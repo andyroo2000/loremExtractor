bd15e89333dfad7821921621a00e42de3bd43d38
Andrew Landry <andrewlandry@gmail.com>
2014-02-21 17:57:08 -0500

    refactor isNumber and lorem into extractor object

--- main.js
+++ main.js
@@ -1,24 +1,48 @@
 var input = "4 Every Dog Must Have His 66 Every Day,";
 
-var lorem = function(text) {
-  var textArray = input.split(' ');
 
-  for (var i = textArray.length - 1; i >= 0; i--) {
-    if (textArray[i].match(/(\d+)$/)) {
-      textArray.splice(i, 1);
+var extractor = {
+  isNumber: function(element) {
+    if (element.match(/(\d+)$/)) {
+      return true;
     }
+  },
+  lorem: function(text) {
+    var textArray = input.split(' ');
+
+    for (var i = textArray.length - 1; i >= 0; i--) {
+      if (this.isNumber(textArray[i])) {
+        textArray.splice(i, 1);
+      }
+    }
+
+    var textObj = {
+      allWords: textArray
+    };
+
+    return textObj;
   }
-
-  // return textArray;
-  var textObj = {
-    allWords: textArray
-  };
-
-  return textObj;
-
 };
 
-output = lorem(input);
+
+// var lorem = function(text) {
+//   var textArray = input.split(' ');
+// 
+//   for (var i = textArray.length - 1; i >= 0; i--) {
+//     if (extractor.isNumber(textArray[i])) {
+//       textArray.splice(i, 1);
+//     }
+//   }
+// 
+//   var textObj = {
+//     allWords: textArray
+//   };
+// 
+//   return textObj;
+// 
+// };
+
+output = extractor.lorem(input);
 
 var fs = require('fs');
 fs.writeFile("output.json", JSON.stringify(output), function(err) {
