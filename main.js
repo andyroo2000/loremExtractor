51e165c43a34f28bed22bede46f20945afeede7e
Andrew Landry <andrewlandry@gmail.com>
2014-02-23 18:03:37 -0500

    use regex to sanitize text

--- main.js
+++ main.js
@@ -1,31 +1,17 @@
-var input = "4 Every dog Must, Have his 66 Every Day!";
+var input = "4 Every dog   Must,  Have his 66 Every Day!";
 
-var extractor = {
-  // returns true if there is a digit present
-  isNumber: function(element) {
-    if (element.match(/(\d+)$/)) {
-      return true;
-    }
-  },
-
-  // returns an object containing arrays of text
-  lorem: function(text) {
-    var textArray = input.split(' ');
-    for (var i = textArray.length - 1; i >= 0; i--) {
-      // remove punctuation
-      textArray[i] = textArray[i].replace(/[,!?()]/gm, '');
-
-      // remove strings that contain digits
-      if (this.isNumber(textArray[i])) {
-        textArray.splice(i, 1);
-      }
-    }
-    return textArray;
-  }
+var lorem = function(text) {
+  // remove punctuation and digits
+  text = text.replace(/[,!?()\d]/gm, '');
+  // remove extra whitespace from body
+  text = text.replace(/\s\s*\b/gm, ' ');
+  // remove whitespace from beginning of text so that there aren't empty array items
+  text = text.replace(/^\s*/gm, '');
+  return text.split(' ');
 };
 
 // sets the output
-output = extractor.lorem(input);
+var output = lorem(input);
 
 // writes the output to a file
 var fs = require('fs');
