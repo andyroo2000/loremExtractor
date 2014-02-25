274f13bb84b00d4fbac75fcc033f3e04341f8ea1
Andrew Landry <andrewlandry@gmail.com>
2014-02-24 21:39:16 -0500

    remove unnecessary comments

--- main.js
+++ main.js
@@ -2,13 +2,10 @@ var fs = require('fs');
 var es = require('event-stream');
 var JSONStream = require('JSONStream');
 
-fs.createReadStream('input/input.txt')      // Read File
-  .pipe(es.replace(/[,!?()=:"\.\d]/gm, '')) // remove punctuation and digits
-  .pipe(es.replace(/\s\s*\b/gm, ' '))       // remove extra whitespace from body
-  .pipe(es.replace(/^\s*/gm, ''))           // remove extra whitespace from beginning of text to get rid of empty arrays
-  .pipe(es.split(' '))                      // split the text in an array
-  .pipe(JSONStream.stringify('[', ',',']'))             // Stringify the array
-  .pipe(fs.createWriteStream("output/output.json"));  // Write the new file
-
-
-  // open='[\n', sep='\n,\n', close='\n]\n')
+fs.createReadStream('input/input.txt')
+  .pipe(es.replace(/[,!?()=:"\.\d]/gm, ''))  // remove punctuation and digits
+  .pipe(es.replace(/\s\s*\b/gm, ' '))        // remove extra whitespace from body
+  .pipe(es.replace(/^\s*/gm, ''))            // remove extra whitespace from beginning of text to get rid of empty arrays
+  .pipe(es.split(' '))                       // split the text in an array
+  .pipe(JSONStream.stringify('[', ',',']'))  // Stringify the array
+  .pipe(fs.createWriteStream("output/output.json"));
